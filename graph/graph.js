739db39743b7602d51cf5511630df3d52b4cdedf
oloomoses <olooine@gmail.com>
2021-06-29 20:18:49 +0300

    Add graph ds

---
+++ graph/graph.js
@@ -0,0 +1,16 @@
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+
+  addVertex(node) {
+    this.adjacentList[node] = []
+    this.numberOfNodes += 1
+  }
+
+  addEdge(node1, node2) {
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+  }
+}
