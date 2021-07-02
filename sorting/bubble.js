563c673027bb6ab6c1bf74f13b8dd2db2aae99ac
oloomoses <olooine@gmail.com>
2021-07-03 02:29:13 +0300

    insertion sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -2,6 +2,7 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
   for(let i = 0; i <= array.length; i++) {
+    let min = i
     for(let j = 0; j <= array.length; j++) {
       if(array[j] > array[j+1]) {
         let temp =  array[j]
--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,20 +1,19 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function selectionSort(array) {
+function insertionSort(array) {
   for(let i = 0; i < array.length; i++) {
-    let min = i;
-    let temp = array[i]
-    for(let j = i + 1; j < array.length; j++) {
-      if(array[j] < array[min]) {
-        min = j
+    if(array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0])
+    } else {
+      for(let j = 1; j < i; j++) {
+        if(array[i] > array[j-1] && array[i] < array[j]) {
+          array.splice(j, 0, array.splice(i, 1)[0])
+        }
       }
     }
-    array[i] = array[min]
-    array[min] = temp
   }
-  return array
 
 }
 
-selectionSort(numbers);
+insertionSort(numbers);
 console.log(numbers);
